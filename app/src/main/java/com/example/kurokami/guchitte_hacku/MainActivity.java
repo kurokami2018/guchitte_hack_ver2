fd547020d4f64e321772150bd240f15b22718a62
konnoyuma <g17930ky@gm.tsuda.ac.jp>
2018-01-14 01:40:25 +0900

    gruLog関係

--- app/src/main/java/com/example/kurokami/guchitte_hacku/MainActivity.java
+++ app/src/main/java/com/example/kurokami/guchitte_hacku/MainActivity.java
@@ -97,13 +97,16 @@ public class MainActivity extends AppCompatActivity {
     }
     void emptyMonthLog(){//ArrayListの中身だけ消去する
         Gson gson=new Gson();
-
         SharedPreferences sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
         SharedPreferences.Editor e = sharedPreferences.edit();
         e.remove("GrumbleData");
         e.commit();
     }
-
+    int getThisMonth(){
+        Calendar cal = Calendar.getInstance();
+        int month = cal.get(Calendar.MONTH)+1; //月を取得
+        return month;
+    }
     void addgruCounter(int month){//gruCounterに++するメソッド引数なし、リターンもなし
         Gson gson=new Gson();
         SharedPreferences sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
@@ -242,6 +245,315 @@ public class MainActivity extends AppCompatActivity {
         img20.startAnimation(fadein_image);
         delGrumble.delGrumble(img20);
 
+    }
+    void setBottle(){
+        Gson gson = new Gson();//ファイル(data)から各月のデータを取得してリストを作成する
+        SharedPreferences sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor e = sharedPreferences.edit();
+        ArrayList gruLog = gson.fromJson(sharedPreferences.getString("GrumbleData", null), new TypeToken<ArrayList<Integer>>(){}.getType());
+
+        int jan=(int)gruLog.get(0);
+        int feb=(int)gruLog.get(1);
+        int mar=(int)gruLog.get(2);
+        int apr=(int)gruLog.get(3);
+        int may=(int)gruLog.get(4);
+        int jun=(int)gruLog.get(5);
+        int jul=(int)gruLog.get(6);
+        int aug=(int)gruLog.get(7);
+        int sep=(int)gruLog.get(8);
+        int oct=(int)gruLog.get(9);
+        int nov=(int)gruLog.get(10);
+        int dec=(int)gruLog.get(11);
+
+
+        if(jan>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(jan>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(jan>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(jan>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(jan>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(jan>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(feb>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(feb>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(feb>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(feb>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(feb>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(feb>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(mar>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(mar>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(mar>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(mar>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(mar>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(mar>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(apr>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(apr>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(apr>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(apr>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(apr>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(apr>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(may>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(may>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(may>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(may>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(may>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(may>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(jun>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(jun>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(jun>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(jun>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(jun>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(jun>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(jul>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(jul>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(jul>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(jul>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(jul>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(jul>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(aug>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(aug>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(aug>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(aug>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(aug>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(aug>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(sep>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(sep>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(sep>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(sep>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(sep>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(sep>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(oct>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(oct>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(oct>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(oct>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(oct>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(oct>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(nov>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(nov>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(nov>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(nov>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(nov>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(nov>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
+        if(dec>9){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        if(dec>19){
+            ImageView imageView2 = (ImageView)findViewById(R.id.imageView6);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+        if(dec>29){
+            ImageView imageView3 = (ImageView)findViewById(R.id.imageView7);
+            imageView3.setVisibility(View.VISIBLE);
+        }
+        if(dec>39){
+            ImageView imageView4 = (ImageView)findViewById(R.id.imageView8);
+            imageView4.setVisibility(View.VISIBLE);
+        }
+        if(dec>49){
+            ImageView imageView5 = (ImageView)findViewById(R.id.imageView9);
+            imageView5.setVisibility(View.VISIBLE);
+        }
+        if(dec>59){
+            ImageView imageView6 = (ImageView)findViewById(R.id.imageView10);
+            imageView6.setVisibility(View.VISIBLE);
+        }
     }
 
 }

cec8c7d574157d44d4b6540b1ea424786d7bd1ad
mina-inoue <g17916im@gm.tsuda.ac.jp>
2018-12-14 18:19:55 +0900

    parry!!yeah!

--- app/src/main/java/com/example/kurokami/guchitte_hacku_ver2/Parry.java
+++ app/src/main/java/com/example/kurokami/guchitte_hacku_ver2/Parry.java
@@ -1,5 +1,25 @@
 package com.example.kurokami.guchitte_hacku_ver2;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.drawable.AnimationDrawable;
+import android.widget.ImageButton;
+import android.view.View;
 
 public class Parry extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parry);
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.image_button);
+        final AnimationDrawable animationDrawable = (AnimationDrawable) imageButton.getDrawable();
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                animationDrawable.start();
+            }
+        });
+    }
 }
